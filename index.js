5f7e0ad6cccc3e8db9b9e7fdb93157e912df43eb
BeveledCube <cubebeveled@gmail.com>
2023-10-16 19:24:13 +0200

    Added request logging

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Use the default port or a custom one
 
+// Custom middleware to log requests
+app.use((req, res, next) => {
+  // Log information about the incoming request
+  console.log(`Request: ${req.method} ${req.url}`);
+  next(); // Continue to the next middleware or route
+});
+
 // Serve all files from the project directory
 app.use(express.static(__dirname));
 
