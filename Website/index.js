8c3111be82ee9bfb3ef39102adbdcb47d820b11e
CubeBeveled <cubebeveled@gmail.com>
2024-01-10 18:20:42 +0100

    u dont need a .env file for a port

--- Website/index.js
+++ Website/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = process.env.PORT || 3000; // Use the default port or a custom one
+const port = 3000; // Use the default port or a custom one
 const folderName = "eng"
 
 // Custom middleware to log requests
