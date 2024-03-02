10510fff3457cb1a3438589ed97305df3bc4ebe6
CubeBeveled <cubebeveled@gmail.com>
2024-03-02 17:07:32 +0100

    Changed enpoint

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/test", (req, res) => res.send("Express on Vercel"));
 
 app.listen(port, () => console.log(`Server ready on port ${port}.`));
 
