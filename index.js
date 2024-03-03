7564d1ab79518b049c76332fe086e881e17f35fe
CubeBeveled <cubebeveled@gmail.com>
2024-03-03 15:11:24 +0100

    removed API folder

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const port = 3000;
 
 api.use(express.json())
 
-api.get("/", (req, res) => res.sendFile("/api/main.html"));
+api.get("/", (req, res) => res.sendFile("main.html"));
 
 api.get("/eng/random", (req, res) => {
-  const files = fs.readdirSync(path.join(__dirname, "../eng"));
+  const files = fs.readdirSync(path.join(__dirname, "eng"));
 
   if (files.length === 0) {
     res.send("There are no memes in the folder :(");
   } else {
-    res.sendFile(path.join(__dirname, `../eng/${files[Math.floor(Math.random() * files.length)]}`))
+    res.sendFile(path.join(__dirname, `eng/${files[Math.floor(Math.random() * files.length)]}`))
   }
 });
 
